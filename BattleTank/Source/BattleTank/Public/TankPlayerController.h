3d8f844ce866b7bfc56accf2702aebb03062913b
mrueda020 <mrueda020@hotmail.com>
2019-03-13 11:27:04 -0600

    Using LineTraceSingleByChannel

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -31,5 +31,9 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	float CrosshairYLocation = 0.3333f;
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000;
 	
+	bool GetLookDirection(FVector2D ScreenLocation, FVector & LookDirection) const;
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
 };
